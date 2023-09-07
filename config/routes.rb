85994fe6fc3618aa8d90e4d04ca88bc7a3b67442
tombom <inoulenkon@gmail.com>
2023-09-07 06:01:09 +0530

    Add controller for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  # get 'landing_page/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
+  root "landing_page#index"
+  
   resources :recipes
   resources :foods
   resources :recipe_foods
