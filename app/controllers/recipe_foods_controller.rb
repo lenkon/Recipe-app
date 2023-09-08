582bf05b2ebc80e49417d9d88348ac31fd9ac782
tombom <inoulenkon@gmail.com>
2023-09-08 11:37:26 +0530

    Add modify food item in recipe list

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,6 +1,6 @@
 class RecipeFoodsController < ApplicationController
   def index
-    @foods = current_user.recipes_foods.select(:food_id, 'SUM(quantity) as quantity').group(:food_id, :quantity)
+    @foods = current_user.recipe_foods.select(:food_id, 'SUM(quantity) as quantity').group(:food_id, :quantity)
     @sum = 0
     @foods.each do |food|
       @sum += food.quantity * food.food.price
@@ -13,26 +13,46 @@ class RecipeFoodsController < ApplicationController
     @foods.each do |food|
       @food_items << [food.name, food.id]
     end
-    @recipes = current_user.recipes
-    @recipe_items = []
-    @recipes.each do |recipe|
-      @recipe_items << [recipe.name, recipe.id]
-    end
+    @recipe = current_user.recipes.find(params[:recipe_id])
   end
 
   def show; end
 
+
   def create
-    @recipe_foods = RecipeFood.new(recipe_foods_params)
+    @recipe_foods = RecipeFood.where('food_id = :x and recipe_id = :y',
+                                       x: recipe_foods_params[:food_id], y: recipe_foods_params[:recipe_id])
+
+    if @recipe_foods.size.zero?
+      @recipe_foods = RecipeFood.new(recipe_foods_params)
+    else
+      @recipe_foods.update(recipe_foods_params)
+      redirect_to recipe_url(@recipe_foods[0].recipe) and return
+    end
 
     respond_to do |format|
       if @recipe_foods.save
         format.html { redirect_to recipe_url(@recipe_foods.recipe), notice: 'Ingredient saved successfully.' }
         format.json { render :show, status: :created, location: @recipe_foods.recipe }
       else
-        format.html { redirect_to new_recipe_foods_url, notice: 'Ingredient not saved.' }
+        format.html { redirect_to new_recipe_recipe_food_url, notice: 'Ingredient not saved.' }
         format.json { render json: @recipe_foods.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def edit
+    @recipe_food = RecipeFood.find(params[:id])
+  end
+
+  def update
+    @recipe_food = RecipeFood.find(params[:id])
+    if @recipe_food.update(recipe_foods_params)
+      flash[:succes] = 'Recipe food updated successfully'
+      redirect_to recipe_path(@recipe_food.recipe)
+    else
+      flash[:error] = 'Recipe food update failed'
+      render :edit
     end
   end
 
